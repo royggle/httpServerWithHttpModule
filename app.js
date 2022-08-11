3d67097bf32e6f4984033b5c0e4267333814c946
royggle <younoroy@gmail.com>
2022-08-11 14:36:35 +0900

    [ADD] 게시물 삭제 엔드포인트

--- app.js
+++ app.js
@@ -64,22 +64,58 @@ const createUser = (req, res) => {
 };
 
 const searchPost = (req, res) => {
-  res.json({ data: posts });
+  let newPost = posts.map((post) => {
+    post.userId;
+    const user = users.find((user) => post.userId == user.id);
+
+    return {
+      ...post,
+      userName: user.name,
+    };
+  });
+  // "id": 1,
+  // "title": "간단한 HTTP API 개발 시작!",
+  // "content": "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+  // "userId": 1,
+  // "userName": "Rebekah Johnson"
+  newPost = res.json({ data: newPost });
 };
 
 const updatePost = (req, res) => {
   const post = req.body.data;
-  console.log("before: ", posts);
+  // posts.forEach((element, i) =>{
+  //   element[i]
+  // })
   for (let i = 0; i < posts.length; i++) {
     if (posts[i].id === post.id) {
       posts[i].title = post.title;
       posts[i].content = post.content;
-      posts[i].userId = post.userId;
     }
   }
-  console.log("after: ", posts);
 
-  res.json({ message: "postUpdated" });
+  const updatePost = posts.map((post) => {
+    post.userId;
+    const user = users.find((user) => post.userId == user.id);
+
+    return {
+      ...post,
+      userName: user.name,
+    };
+  });
+
+  res.json({ data: updatePost });
 };
 
-module.exports = { createUser, createPost, searchPost, updatePost };
+const deletePost = (req, res) => {
+  const post = req.body.data;
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].id === post.id) {
+      posts.splice(i, 1);
+      i--;
+    }
+  }
+
+  res.json({ message: "postingDeleted" });
+};
+
+module.exports = { createUser, createPost, searchPost, updatePost, deletePost };
